841c63dc147b586924bceb909a28260d436f42cc
zonoise <zonoise@gmail.com>
2021-12-09 01:12:47 +0900

    GatsbyImageでimage表示

---
+++ src/components/card.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { GatsbyImage } from "gatsby-plugin-image";
+import * as styles from "../styles/components/card.module.scss";
+const Card = ({ image, text }) => {
+  return (
+    <div className={styles.root}>
+      <div className={styles.title}>{text}</div>
+      {<GatsbyImage image={image} alt={text} />}
+    </div>
+  );
+};
+
+export default Card;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,46 @@
-import * as React from 'react'
-import Layout from '../components/layout'
-import { graphql } from "gatsby"
+import * as React from "react";
+import Layout from "../components/layout";
+import { graphql } from "gatsby";
+import Card from "../components/card";
+import { getImage } from "gatsby-plugin-image"
 
-const IndexPage = ({data}) => {
+const IndexPage = ({ data }) => {
+
   return (
     <Layout pageTitle="Home Page">
+      {data.allContentfulPost.edges.map((edge) => {
 
-      {data.allContentfulPost.edges.map(edge =>
-        <p>{edge.node.title}</p>
-      )}
-
+        const image = getImage(edge.node.image);
+        return (
+          <Card key={edge.node.id} text={edge.node.title} image={image} />
+        )
+        
+      })}
     </Layout>
-  )
-}
+  );
+};
 
 export const query = graphql`
-    query allContentfulPost {
-      allContentfulPost {
-        edges {
-          node {
+  query allContentfulPost {
+    allContentfulPost {
+      edges {
+        node {
+          title
+          slug
+          image {
             title
-            slug
+            description
+            gatsbyImageData(
+              width: 200
+            )
+            file {
+              url
+            }
           }
         }
       }
     }
-`
+  }
+`;
 
-export default IndexPage
+export default IndexPage;
